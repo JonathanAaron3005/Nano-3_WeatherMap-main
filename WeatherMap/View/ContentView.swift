7f2fc93a41dc2a266c473d770cf6be4534703e20
Jonathan Aaron Wibawa <aaron.wibawa@gmail.com>
2024-07-11 15:51:08 +0700

    change transportType & show ETA

--- WeatherMap/View/ContentView.swift
+++ WeatherMap/View/ContentView.swift
@@ -10,6 +10,20 @@ import WeatherKit
 import CoreLocation
 import MapKit
 
+enum TransportType: String, Hashable {
+    case automobile
+    case walking
+    
+    var mkTransportType: MKDirectionsTransportType {
+        switch self {
+        case .automobile:
+            return .automobile
+        case .walking:
+            return .walking
+        }
+    }
+}
+
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let manager = CLLocationManager()
     @Published var location: CLLocation?
@@ -43,71 +57,83 @@ struct ContentView: View {
     @State private var routeDisplaying = false
     @State private var route: MKRoute?
     @State private var routeDestination: MKMapItem?
+    @State private var transportType: TransportType = .automobile
     
     var body: some View {
-        Map(position: $cameraPosition, selection: $mapSelection) {
-            //            Marker("My Location", systemImage: "paperplane", coordinate: .userLocation)
-            //                .tint(.blue)
-            //            UserAnnotation()
-            Annotation("My Location", coordinate: .userLocation) {
-                ZStack {
-                    Circle()
-                        .frame(width: 32, height: 32)
-                        .foregroundStyle(.blue.opacity(0.25))
-                    Circle()
-                        .frame(width: 20, height: 20)
-                        .foregroundStyle(.white)
-                    Circle()
-                        .frame(width: 12, height: 12)
-                        .foregroundStyle(.blue)
-                }
+        VStack {
+            Picker("Transport Type", selection: $transportType) {
+                Text("Automobile").tag(TransportType.automobile)
+                Text("Walking").tag(TransportType.walking)
             }
-            ForEach(results, id: \.self) { item in
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            .onChange(of: transportType) { newValue in
                 if routeDisplaying {
-                    if item == routeDestination {
-                        let placemark = item.placemark
-                        Marker(placemark.name ?? "", coordinate: placemark.coordinate)
-                    }
-                } else {
-                    let placemark = item.placemark
-                    Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                    fetchRoute()
                 }
             }
             
-            if let route {
-                MapPolyline(route.polyline)
-                    .stroke(.blue, lineWidth: 6)
+            Map(position: $cameraPosition, selection: $mapSelection) {
+                Annotation("My Location", coordinate: .userLocation) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.blue.opacity(0.25))
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .foregroundStyle(.white)
+                        Circle()
+                            .frame(width: 12, height: 12)
+                            .foregroundStyle(.blue)
+                    }
+                }
+                ForEach(results, id: \.self) { item in
+                    if routeDisplaying {
+                        if item == routeDestination {
+                            let placemark = item.placemark
+                            Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                        }
+                    } else {
+                        let placemark = item.placemark
+                        Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                    }
+                }
+                
+                if let route {
+                    MapPolyline(route.polyline)
+                        .stroke(.blue, lineWidth: 6)
+                }
             }
-        }
-        .overlay(alignment: .top) {
-            TextField("Search for a location...", text: $searchText)
-                .font(.subheadline)
-                .padding(12)
-                .background(.white)
-                .padding()
-                .shadow(radius: 10)
-        }
-        .onSubmit(of: /*@START_MENU_TOKEN@*/.text/*@END_MENU_TOKEN@*/) {
-            Task { await searchPlaces() }
-        }
-        .onChange(of: getDirections, { oldValue, newValue in
-            if newValue {
-                fetchRoute()
+            .overlay(alignment: .top) {
+                TextField("Search for a location...", text: $searchText)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(.white)
+                    .padding()
+                    .shadow(radius: 10)
             }
-        })
-        .onChange(of: mapSelection, { oldValue, newValue in
-            showDetails = newValue != nil
-        })
-        .sheet(isPresented: $showDetails) {
-            LocationDetailsView(mapSelection: $mapSelection, show: $showDetails, getDirections: $getDirections)
-                .presentationDetents([.height(340)])
-                .presentationBackgroundInteraction(.enabled(upThrough: .height(340)))
-                .presentationCornerRadius(12)
-        }
-        .mapControls {
-            MapCompass()
-            MapPitchToggle()
-            MapUserLocationButton()
+            .onSubmit(of: .text) {
+                Task { await searchPlaces() }
+            }
+            .onChange(of: getDirections) { oldValue, newValue in
+                if newValue {
+                    fetchRoute()
+                }
+            }
+            .onChange(of: mapSelection) { oldValue, newValue in
+                showDetails = newValue != nil
+            }
+            .sheet(isPresented: $showDetails) {
+                LocationDetailsView(mapSelection: $mapSelection, show: $showDetails, getDirections: $getDirections)
+                    .presentationDetents([.height(340)])
+                    .presentationBackgroundInteraction(.enabled(upThrough: .height(340)))
+                    .presentationCornerRadius(12)
+            }
+            .mapControls {
+                MapCompass()
+                MapPitchToggle()
+                MapUserLocationButton()
+            }
         }
     }
 }
@@ -127,12 +153,16 @@ extension ContentView {
             let request = MKDirections.Request()
             request.source = MKMapItem(placemark: .init(coordinate: .userLocation))
             request.destination = mapSelection
+            request.transportType = transportType.mkTransportType
             
             Task {
                 let result = try? await MKDirections(request: request).calculate()
                 route = result?.routes.first
                 
                 if let route {
+                    let eta = route.expectedTravelTime
+                    print("Estimated Travel Time: \(eta / 60) minutes")
+                    
                     for step in route.steps {
                         let coordinate = step.polyline.coordinate
                         print("longitude: \(coordinate.longitude), latitude: \(coordinate.latitude)")
@@ -163,7 +193,7 @@ func fetchWeatherData(for location: CLLocation) async {
         let weather = try await weatherService.weather(for: location)
         
         if let hourlyForecast = weather.hourlyForecast.first {
-            var precipitationChance = hourlyForecast.precipitationChance
+            let precipitationChance = hourlyForecast.precipitationChance
             print("Precipitation chance at \(location.coordinate.latitude), \(location.coordinate.longitude): \(precipitationChance * 100)%")
         } else {
             print("No precipitation data available at \(location.coordinate.latitude), \(location.coordinate.longitude)")
