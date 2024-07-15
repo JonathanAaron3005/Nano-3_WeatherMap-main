90a29e8c4c568a0b928f8cf3b6be45bd76839936
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-07-15 17:01:33 +0700

    add precipitation to weather prediction

--- WeatherMap/View/TestView.swift
+++ WeatherMap/View/TestView.swift
@@ -12,7 +12,10 @@ import CoreLocation
 struct TestView: View {
     @StateObject private var viewModel = WeatherViewModel()
     @State private var selectedDate = Date()
-    private let location = CLLocation(latitude: 37.7749, longitude: -122.4194) // Example location
+    
+   //  -6.212922, 106.848723
+    // -2.556158, 140.692735
+    private let location = CLLocation(latitude: -2.556158, longitude: 140.692735) // Example location
 
     var body: some View {
         VStack {
@@ -24,6 +27,13 @@ struct TestView: View {
                 Text("Weather at \(selectedDate):")
                 Text("Temperature: \(weather.temperature.value, specifier: "%.1f") \(weather.temperature.unit.symbol)")
                 Text("Condition: \(weather.condition.description)")
+                
+                if let precipitationProbability = viewModel.precipitationProbability {
+                    Text("Precipitation Probability: \(Int(precipitationProbability * 100))%")
+                }
+                if let precipitationAmount = viewModel.precipitationAmount {
+                    Text("Precipitation Amount: \(precipitationAmount.converted(to: .millimeters).value) mm")
+                }
             } else if let errorMessage = viewModel.errorMessage {
                 Text(errorMessage)
                     .foregroundColor(.red)
