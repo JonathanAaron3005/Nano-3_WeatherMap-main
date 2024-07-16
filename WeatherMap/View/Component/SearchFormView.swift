a3cc4b30758315fef65133b93513539c45192b93
Hendra Lijaya <hendralijaya@gmail.com>
2024-07-16 17:27:51 +0700

    Resolving merge conflict and functionality start already good

--- WeatherMap/View/Component/SearchFormView.swift
+++ WeatherMap/View/Component/SearchFormView.swift
@@ -6,88 +6,160 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct SearchFormView: View {
     @Binding var searchText: String
     @Binding var additionalSearchTexts: [String]
     @Binding var searchTextIndex: Int?
     @ObservedObject var locationManager: LocationManager
-    //    var searchPlaces: (String) async -> Void
+    @Binding var selectedResult: [MKMapItem]
+    @Binding var myLocation: MKMapItem?
+//    var searchPlaces: (String) async -> Void
+    
+    @State private var showYourLocationSheet = false
+    @State private var showAddDestinationSheet = false
+    
+    @State var title = ""
+    
+    @Binding var date: Date
+    @Binding var transportType: TransportType
+    
+    @Binding var routeDisplaying: Bool
+    @Binding var routes: [MKRoute]
+    
+    var fetchRoute: () -> Void
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerSize: CGSize(width: 45, height: 45))
-                .fill(.clear)
-                .background(.gray.opacity(0.25))
-                .cornerRadius(20)
+            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                .fill(.white)
+                .frame(height: 200)
                 .padding()
-            RoundedRectangle(cornerSize: CGSize(width: 45, height: 45))
-                .fill(.clear)
-                .frame(width: 320, height: 100)
-                .background(.gray.opacity(0.25))
-                .cornerRadius(20)
-                .padding()
-            VStack {
-                Form {
-                    TextField("Current Location", text: $locationManager.locationName)
-                        .font(.subheadline)
-                        .background(Color.clear)
-                        .padding(.vertical, 8)
-                    
-                    TextField("Search for a location...", text: $searchText)
-                        .font(.subheadline)
-                        .background(Color.clear)
-                        .padding(.vertical, 8)
-                        .onSubmit {
-                            //Task { await searchPlaces(searchText) }
-                        }
-                    
-                    ForEach(0..<additionalSearchTexts.count, id: \.self) { index in
-                        TextField("Search for a location...", text: Binding(
-                            get: { additionalSearchTexts[index] },
-                            set: { newValue in
-                                additionalSearchTexts[index] = newValue
-                                searchTextIndex = index
-                            }
-                        ))
-                        .font(.subheadline)
-                        .background(Color.clear)
-                        .onSubmit {
-                            if let index = searchTextIndex {
-                                searchAndAddPlaces(index: index)
-                            }
-                        }
-                    }
-                }
-                .background(Color.clear)
-                .scrollContentBackground(.hidden)
-                .padding()
+                .padding(.bottom, 16)
+                .shadow(radius: 10)
+            
+            NavigationStack {
                 
-                Button(action: {
-                    additionalSearchTexts.append("")
-                }) {
-                    Text("Add Stop")
-                        .font(.subheadline)
-                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 30)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(30)
+                VStack {
+                    VStack {
+                        VStack {
+                            HStack {
+                                Image(systemName: "location.north.circle.fill")
+                                    .symbolRenderingMode(.multicolor)
+                                Text(myLocation?.name ?? "Your Location")
+                                Spacer()
+                            }
+                            .onTapGesture {
+                                title = "Your Location"
+                                showAddDestinationSheet.toggle()
+                            }
+                            Divider()
+                            HStack {
+                                Image(systemName: "mappin.circle.fill")
+                                    .symbolRenderingMode(.multicolor)
+                                Text(selectedResult.first?.placemark.name ?? "Add Destination")
+                                Spacer()
+                            }
+                            .onTapGesture {
+                                title = "Add Destination"
+                                showAddDestinationSheet.toggle()
+                            }
+
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 24)
+                    }
+                    .overlay(
+                        HStack {
+                            Spacer()
+                            Button {
+                                //ACTION
+                            } label: {
+                                Text("Add Stop")
+                                    .padding(8)
+                            }
+                            .background(
+                                Capsule()
+                                    .fill(.white)
+                            )
+                            .padding()
+                        }
+                    )
+                    .background(
+                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                            .fill(.lightGrayBackground)
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 16)
+                    .sheet(isPresented: $showAddDestinationSheet) {
+                        DestinationSheet(title: $title, selectedResult: $selectedResult, fetchRoute: fetchRoute, myLocation: $myLocation)
+                            .presentationDragIndicator(.visible)
+                    }
+                    CustomPicker(date: $date, transportType: $transportType, routeDisplaying: $routeDisplaying, routes: $routes, fetchRoute: fetchRoute)
+                        .padding(.trailing, 15)
+                        .padding(.top, -28)
+                        .padding(.leading, -5)
                 }
-                .offset(x: 100, y: -10)
             }
+                
         }
-        .frame(height: 200)
+//        Form {
+//            TextField("Current Location", text: $locationManager.locationName)
+//                .font(.subheadline)
+//                .background(Color.clear)
+//            
+//            TextField("Search for a location...", text: $searchText)
+//                .font(.subheadline)
+//                .background(Color.clear)
+////                .onSubmit {
+////                    Task { await searchPlaces(searchText) }
+////                }
+//            
+//            ForEach(0..<additionalSearchTexts.count, id: \.self) { index in
+//                TextField("Search for a location...", text: Binding(
+//                    get: { additionalSearchTexts[index] },
+//                    set: { newValue in
+//                        additionalSearchTexts[index] = newValue
+//                        searchTextIndex = index
+//                    }
+//                ))
+//                .font(.subheadline)
+//                .background(Color.clear)
+//                .onSubmit {
+//                    if let index = searchTextIndex {
+//                        searchAndAddPlaces(index: index)
+//                    }
+//                }
+//            }
+//            Button(action: {
+//                additionalSearchTexts.append("")
+//            }) {
+//                Text("Add Stop")
+//                    .font(.subheadline)
+//                    .padding()
+//                    .background(Color.blue)
+//                    .foregroundColor(.white)
+//                    .cornerRadius(8)
+//            }
+//        }
+//        .background(.white)
+//        .cornerRadius(20)
+//        .frame(height: 200)
+//        .padding()
+//        .shadow(radius: 10)
     }
     
     func searchAndAddPlaces(index: Int) {
         let searchText = additionalSearchTexts[index]
-        //        Task {
-        //            await searchPlaces(searchText)
-        //        }
+        Task {
+//            await searchPlaces(searchText)
+        }
     }
 }
 
 #Preview {
-    SearchFormView(searchText: .constant("tes"), additionalSearchTexts: .constant([]), searchTextIndex: .constant(1), locationManager: LocationManager())
+    SearchFormView(searchText: .constant("tes"), additionalSearchTexts: .constant(["tes"]), searchTextIndex: .constant(1), locationManager: LocationManager(), selectedResult: .constant([MKMapItem()]), myLocation: .constant(MKMapItem()), date: .constant(Date()), transportType: .constant(.automobile), routeDisplaying: .constant(false), routes: .constant([MKRoute]())) {
+        print("tes")
+    }
 }
-
